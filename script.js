4af2f4f11a4451d3ef40fdda6a3ae2b6d25e7276
KartikeyJoshi916 <95592393+KartikeyJoshi916@users.noreply.github.com>
2025-01-08 14:13:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
-//your JS code here. If required.
-function display(){
-		let fName=document.getElementById("firstName").value;
-		let lName=document.getElementById("lastName").value;
-		let number=document.getElementById("number").value;
-		let email=document.getElementById("email").value;
-	 let message = "First Name: " + fName + " "+
-                  "Last Name: " + lName + " "+
-                  "Phone Number: " + number + " "+
+function display() {
+    let fName = document.getElementById("firstName").value;
+    let lName = document.getElementById("lastName").value;
+    let number = document.getElementById("number").value;
+    let email = document.getElementById("email").value;
+
+    let message = "First Name: " + fName + " " +
+                  "Last Name: " + lName + " " +
+                  "Phone Number: " + number + " " +
                   "Email ID: " + email;
 
-	alert(message);
+    alert(message);
 }
