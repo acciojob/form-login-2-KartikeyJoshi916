5fbe5429481fad1a11d05ce5dc02299869765f8d
KartikeyJoshi916 <95592393+KartikeyJoshi916@users.noreply.github.com>
2025-01-08 14:01:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+function display(){
+	alert(
+		let fName=document.getElementById("firstName");
+		let lName=document.getElementById("lastName");
+		let number=document.getElementById("number");
+		let email=document.getElementById("email");
+		console.log("First Name: "+fName.value+
+					"Last Name: "+lName.value+
+					"Phone Number: "+number.value+
+					"Email ID: "+email.value
+				   );
+	);
+}
